f7951748f398854acb317c110074fe0b4a959f93
marcos <marcosdeixx@gmail.com>
2025-04-25 11:19:07 -0300

    feat: import Spring Project 0.0.1

---
+++ src/test/java/com/vanguard/vanguard/VanguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanguard.vanguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
